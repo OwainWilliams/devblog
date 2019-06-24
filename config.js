52456b7672add75373556a27372cb505da4ccf7c
Owain Williams <owaingdwilliams@gmail.com>
2019-06-24 20:53:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Another Dev Blog', // Required
+  author: 'Owain Williams', // Required
+  description: 'Umbraco Developer playing with other techonology',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://owain.codes',
+    github: 'https://github.com/owainwilliams',
+    twitter: 'https://twitter.com/scottishcoder',
+    linkedin: 'https://ca.linkedin.com/in/owainwilliams',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
